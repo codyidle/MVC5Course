84e36debbab416be55c887e6660e9af32bbe3d0e
codyidle <cody@edetw.com>
2016-03-28 11:02:16 +0800

    3/28第二練習回報後簽入

--- MVC5Course/Models/ProductRepository.cs
+++ MVC5Course/Models/ProductRepository.cs
@@ -10,7 +10,25 @@ namespace MVC5Course.Models
         {
             return this.All().FirstOrDefault(p => p.ProductId == id);
         }
-	}
+
+        public override IQueryable<Product> All()
+        {
+            return base.All().Where(p => p.IsDelete == false);
+        }
+
+        public override void Delete(Product entity)
+        {
+            entity.IsDelete = true;
+        }
+
+        public IQueryable<Product> All(bool isAll = false)
+        {
+            if (isAll)
+                return base.All();
+            else
+                return this.All();
+        }
+    }
 
 	public  interface IProductRepository : IRepository<Product>
 	{
