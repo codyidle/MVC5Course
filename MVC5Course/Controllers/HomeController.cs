342bb4f54044c6b344840c391650cf6c8639a21e
codyidle <cody@edetw.com>
2016-04-06 14:28:59 +0800

    4/6 第四次簽入 (自訂錯誤頁面)

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -13,8 +13,14 @@ namespace MVC5Course.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HandleError(ExceptionType = typeof(InvalidOperationException),View ="Error2")]
+        public ActionResult About(string name)
         {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("沒有帶入差數");
+
+            throw new InvalidOperationException("其他錯誤");
+
             ViewBag.Message = "Your application description page.";
 
             return View();
